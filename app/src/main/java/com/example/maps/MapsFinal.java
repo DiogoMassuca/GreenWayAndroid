6fba2ab3436c832b90484308de93a7fd92d3c4c5
Diogo Massuça <90829219+DiogoMassuca@users.noreply.github.com>
2021-12-07 16:27:53 +0000

    c

--- app/src/main/java/com/example/maps/MapsFinal.java
+++ app/src/main/java/com/example/maps/MapsFinal.java
@@ -2,19 +2,41 @@ package com.example.maps;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
 import com.example.maps.databinding.ActivityMapsFinalBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsFinal extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap mMap;
     ActivityMapsFinalBinding binding;
+    MapView mapView;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,17 +45,131 @@ public class MapsFinal extends AppCompatActivity implements OnMapReadyCallback {
         binding = ActivityMapsFinalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        MapView mapView = findViewById(R.id.mapView);
+        mapView = findViewById(R.id.mapView);
         mapView.getMapAsync(this);
+        mapView.onCreate(savedInstanceState);
+
+        // 1 - Criar o location Manager para ir buscar
+        // a localização onde o nosso device está
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // 2 - Criar um location Listener para
+        // detetar mudanças na nossa localização
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(userLocation).title("Here"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 16));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        // 3 - Detetar se o utilizador nos deu
+        // permissões para obter a localização
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+        else
+        {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0,
+                    0,
+                    locationListener
+            );
+        }
+    }
+    // 4 - Verificar se temos permissões ao executar a nossa Activity
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0,
+                        0,
+                        locationListener
+                );
+            }
+        }
     }
 
+
+    // OnMapReadyCallBack Methods
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        MapsInitializer.initialize(this);
         mMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    // Change Activities
     public void onClickGoToProfile(View v){
         Intent intentProfile = new Intent(MapsFinal.this, ProfileActivity.class);
         startActivity(intentProfile);
